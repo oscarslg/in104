a2c00e9e36d125d1d94457e15ea5463c154e20bb
oscarslg <48727306+oscarslg@users.noreply.github.com>
2019-04-16 16:18:17 +0200

    Add files via upload

--- constantes.py
+++ constantes.py
@@ -1,31 +1,20 @@
 """Constantes du jeu de Labyrinthe Donkey Kong"""
 
-
 #Paramètres de la fenêtre
-
-nombre_sprite_cote = 15
-
+nombre_sprite_cote = 20
 taille_sprite = 30
-
 cote_fenetre = nombre_sprite_cote * taille_sprite
 
-
 #Personnalisation de la fenêtre
-
 titre_fenetre = "DK Labyrinthe"
+image_icone = "droite.png"
 
-image_icone = "images/dk_droite.png"
+#Listes des images du jeux
+image_accueil = "th.jpeg"
+image_fond = "fond.jpeg"
 
+image_depart = "depart.png"
+image_arrivee = "arrivee.png"
+image_nourriture = "dogs.png"
 
-#Listes des images du jeu
-
-image_accueil = "images/accueil.png"
-
-image_fond = "images/fond.jpg"
-
-image_mur = "images/mur.png"
-
-image_depart = "images/depart.png"
-
-image_arrivee = "images/arrivee.png"
-
+black = "noir.png"
